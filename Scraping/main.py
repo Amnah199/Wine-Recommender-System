fe45bde89662155cb01df75330c4bfdd71630aad
Zoran Lisovac <zoran.lisovac@gmail.com>
2022-01-01 19:47:48 +0100

    Fixed bugs in Divino wines scraping

--- Scraping/main.py
+++ Scraping/main.py
@@ -10,7 +10,7 @@ import scrape_wines_divino
 # https://www.wein-direktimport.de/alle-weine/typ/bioweine/?p=1&o=1&n=96 - bio
 # https://www.wein-direktimport.de/alle-weine/typ/grosse-gewaechse/?p=1&o=1&n=96
 
-scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/schaumweine/?p=1&o=3&n=96', 'C:/Users/zoran/Desktop/sparkling-di.xlsx')
+#scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/schaumweine/?p=1&o=3&n=96', 'C:/Users/zoran/Desktop/sparkling-di.xlsx')
 
 # Divino:
 # https://www.divino.de/blanquette-cremant?sPage=1&sPerPage=48
@@ -19,4 +19,10 @@ scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/al
 # https://www.divino.de/rotwein?p=1&n=48
 # https://www.divino.de/dessertwein?sPage=1&sPerPage=48
 
-#scrape_wines_divino.scrape_wines('https://www.divino.de/dessertwein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/dessert-divino.xlsx')
+headers = ["Name", "Jahrgang", "Rebsorte", "Weingut", "Produktbezeichnung", "Ort", "Passt gut zu", "Herkunftsland", "Anlass", "Größe des Winzers", "Winzer", "Region", "Herstellungsmethode", "Art", "Lagerfähigkeit", "Charakteristik", "Beschreibung"]
+
+scrape_wines_divino.scrape_wines('https://www.divino.de/blanquette-cremant?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/sparkling-divino.xlsx', headers)
+scrape_wines_divino.scrape_wines('https://www.divino.de/weisswein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/white-divino.xlsx', headers)
+scrape_wines_divino.scrape_wines('https://www.divino.de/rosewein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/rose-divino.xlsx', headers)
+scrape_wines_divino.scrape_wines('https://www.divino.de/rotwein?p=1&n=48', 'C:/Users/zoran/Desktop/red-divino.xlsx', headers)
+scrape_wines_divino.scrape_wines('https://www.divino.de/dessertwein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/dessert-divino.xlsx', headers)
