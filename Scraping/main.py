c441b3d0dbc6f914b23601025452d87177dcc866
Zoran Lisovac <zoran.lisovac@gmail.com>
2022-01-18 22:00:56 +0100

    Additional scraping material for divino and di wines

--- Scraping/main.py
+++ Scraping/main.py
@@ -10,7 +10,17 @@ import scrape_wines_divino
 # https://www.wein-direktimport.de/alle-weine/typ/bioweine/?p=1&o=1&n=96 - bio
 # https://www.wein-direktimport.de/alle-weine/typ/grosse-gewaechse/?p=1&o=1&n=96
 
-#scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/schaumweine/?p=1&o=3&n=96', 'C:/Users/zoran/Desktop/sparkling-di.xlsx')
+
+headersDI = ["Name:", "Hersteller:", "Erzeugnis aus:", "Region:",	"Wein:", "Jahrgang:",	"Rebsorte:", "Qualität:", "Alkohol in %:", 	"Geschmack:",	"Allergene:", \
+             "Trinktemperatur in °C:",	"Trinkempfehlung:",	"Dekantieren:",	"Verschluß:",	"Säure g/l:",	"Restzucker g/l:",	"Boden:",	"Essen:",	"Ausbauart:", \
+             "Auszeichnungen:",	"Paßt zu:",	"Lage:",	"Herstellung:",	"Produkt aus biologischem Anbau:",	"Biokontrollstelle:",	"Diverses:",	"Zutaten:",	"Preis:",	"Image:",	"Details-URL:",	"Verkostungsnotiz:"]
+
+scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/weissweine/?p=1&o=1&n=96', 'C:/Users/zoran/Desktop/white-di.xlsx', headersDI)
+scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/rotweine/?p=1&o=1&n=96', 'C:/Users/zoran/Desktop/red-di.xlsx', headersDI)
+scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/roseweine/?p=1&o=1&n=96', 'C:/Users/zoran/Desktop/rose-di.xlsx', headersDI)
+scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/schaumweine/?p=1&o=3&n=96', 'C:/Users/zoran/Desktop/sparkling-di.xlsx', headersDI)
+scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/bioweine/?p=1&o=1&n=96', 'C:/Users/zoran/Desktop/bio-di.xlsx', headersDI)
+scrape_wines_wein_direktimport.scrape_wines('https://www.wein-direktimport.de/alle-weine/typ/grosse-gewaechse/?p=1&o=1&n=96', 'C:/Users/zoran/Desktop/big-plantations-di.xlsx', headersDI)
 
 # Divino:
 # https://www.divino.de/blanquette-cremant?sPage=1&sPerPage=48
@@ -19,10 +29,10 @@ import scrape_wines_divino
 # https://www.divino.de/rotwein?p=1&n=48
 # https://www.divino.de/dessertwein?sPage=1&sPerPage=48
 
-headers = ["Name", "Jahrgang", "Rebsorte", "Weingut", "Produktbezeichnung", "Ort", "Passt gut zu", "Herkunftsland", "Anlass", "Größe des Winzers", "Winzer", "Region", "Herstellungsmethode", "Art", "Lagerfähigkeit", "Charakteristik", "Beschreibung"]
+#headers = ["Name", "Jahrgang", "Rebsorte", "Weingut", "Produktbezeichnung", "Ort", "Passt gut zu", "Herkunftsland", "Anlass", "Größe des Winzers", "Winzer", "Region", "Herstellungsmethode", "Art", "Lagerfähigkeit", "Charakteristik", 'Preis', 'Image', 'Details-URL', "Beschreibung"]
 
-scrape_wines_divino.scrape_wines('https://www.divino.de/blanquette-cremant?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/sparkling-divino.xlsx', headers)
-scrape_wines_divino.scrape_wines('https://www.divino.de/weisswein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/white-divino.xlsx', headers)
-scrape_wines_divino.scrape_wines('https://www.divino.de/rosewein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/rose-divino.xlsx', headers)
-scrape_wines_divino.scrape_wines('https://www.divino.de/rotwein?p=1&n=48', 'C:/Users/zoran/Desktop/red-divino.xlsx', headers)
-scrape_wines_divino.scrape_wines('https://www.divino.de/dessertwein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/dessert-divino.xlsx', headers)
+#scrape_wines_divino.scrape_wines('https://www.divino.de/blanquette-cremant?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/sparkling-divino.xlsx', headers)
+#scrape_wines_divino.scrape_wines('https://www.divino.de/weisswein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/white-divino.xlsx', headers)
+#scrape_wines_divino.scrape_wines('https://www.divino.de/rosewein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/rose-divino.xlsx', headers)
+#scrape_wines_divino.scrape_wines('https://www.divino.de/rotwein?p=1&n=48', 'C:/Users/zoran/Desktop/red-divino.xlsx', headers)
+#scrape_wines_divino.scrape_wines('https://www.divino.de/dessertwein?sPage=1&sPerPage=48', 'C:/Users/zoran/Desktop/dessert-divino.xlsx', headers)
--- Scraping/scrape_wines_wein_direktimport.py
+++ Scraping/scrape_wines_wein_direktimport.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import re
 
 
-def scrape_wines(url, output):
+def scrape_wines(url, output, headers):
     """
     Scrape wines from https://www.wein-direktimport.de
     :param url: Full URL after choosing desired filters and choosing the number of elements per page e.g. https://www.wein-direktimport.de/alle-weine/land/deutschland/?p=1&o=1&n=96&f=1992
@@ -11,7 +12,6 @@ def scrape_wines(url, output):
     :return:
     """
 
-    headers = ['Name:', 'Verkostungsnotiz:']
     data = []
     req = requests.get(url)
     soup = BeautifulSoup(req.text, 'lxml')
@@ -29,7 +29,24 @@ def scrape_wines(url, output):
 
         wines = soup.find_all('div', {"class": "product--info"})
         for wine in wines:
+            price = None
+            try:
+                price = wine.find('div', attrs={"class": 'product--price'}).find('span').text
+                price = re.findall("\d+(?:\.\d+)?,\d+", price)[0]
+            except:
+                price = None
             url_details = wine.find('a', attrs={"class": 'product--image'})['href']
+
+            imageContainer = wine.find('span', attrs={"class": 'image--media'})
+
+            image = None
+            try:
+                if imageContainer:
+                    image = wine.find('img')['src']
+            except:
+                if imageContainer:
+                    image = wine.find('img')['srcset'].split(',')[0]
+
             details = requests.get(url_details)
             soup = BeautifulSoup(details.text, 'lxml')
             name = soup.find('h1', attrs={"class": 'product--title'}).text
@@ -40,18 +57,13 @@ def scrape_wines(url, output):
                 for tag in span.find_all('p'):
                     tasting_note += f' {tag.text}'
 
-            row_data = {'Name:': name, 'Verkostungsnotiz:': tasting_note}
+            row_data = {'Name:': name, 'Verkostungsnotiz:': tasting_note, 'Preis:': price, 'Details-URL:': url_details, 'Image:': image}
             details = soup.find('table', attrs={"class": 'product--properties'})
             details_rows = details.find_all('tr')
             for row in details_rows:
                 row_details = row.find_all('td')
 
                 header = row_details[0].find('span').text
-
-                if header not in headers:
-                    headers.append(header)
-                    headers.append(headers.pop(headers.index('Verkostungsnotiz:')))
-
                 row_data[header] = row_details[1].text
 
             data.append(row_data)
