0dbbf43757beb9a0656b2cbb450d148bff26a242
Dex <mike.da0v9@slmail.me>
2022-01-27 12:42:19 +0100

    fixed recommendation calc

--- WineRecommender_API/recommender_engine/recommender.py
+++ WineRecommender_API/recommender_engine/recommender.py
@@ -2,18 +2,23 @@
 import numpy as np
 
 
-def calcRecs(wines_list, user_profile, num_wines, structure_param = 0.5, tastes_param = 1, ratings_param = 1):
-    user_profile["structure"] = np.array(user_profile["structure"].values())
-    user_profile["tastes"] = np.array(user_profile["tastes"].values())
+def calcRecs(wines_list, user_profile, num_wines = 10, structure_param = 0.5, tastes_param = 1, ratings_param = 1):
+    user_structure = np.asarray(list(user_profile["structure"].values()))
+    user_tastes = np.asarray(list(user_profile["tastes"].values()))
+
     for wine in wines_list:
-        wine["wine_tastes"] = np.array(wine["wine_tastes"].values())
-        wine["wine_structure"] = np.array(wine["wine_structure"].values())
-        wine["wine_rating"] = wine["wine_rating"]/4
-        wine["score"] = (structure_param * sum(user_profile["structure"]*wine["wine_structure"]) + tastes_param * sum(user_profile["tastes"]*wine["wine_tastes"])) * (ratings_param * wine["wine_rating"])
+        wine_tastes = np.asarray(list(wine["wine_tastes"].values()))
+        wine_structure = np.asarray(list(wine["wine_structure"].values()))
+        wine["score"] = (structure_param * np.sum(np.multiply(user_structure, wine_structure)) + tastes_param * np.sum(np.multiply(user_tastes, wine_tastes))) * (ratings_param * float(wine["wine_rating"])/4)
     
-    wines_list = sorted(wines_list, key=lambda k: k["score"])
+    wines_list = sorted(wines_list, key=lambda k: k["score"], reverse=True)
+
+    if num_wines > len(wines_list):
+        num_wines = len(wines_list)
+
     return wines_list[:num_wines-1]
 
+
 def generateProfile(wine_ids):
-    
+
     return user_profile
