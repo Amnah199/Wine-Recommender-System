bf83b3b89f347f95347049b0baf73b1921998f1b
l_sens01 <l_sens01@uni-muenster.de>
2022-01-31 10:31:36 +0100

    changes to scraper

--- vivino scraper/src/wine_search_scraper.py
+++ vivino scraper/src/wine_search_scraper.py
@@ -1,4 +1,5 @@
 import pathlib
+from signal import SIG_DFL
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,7 +14,8 @@ from bs4 import BeautifulSoup
 import time
 import config
 import pandas as pd
-
+import json
+import os
 filePath = pathlib.Path(__file__).parent.resolve()
 
 
@@ -32,35 +34,54 @@ driver = webdriver.Chrome(options=chrome_options,
 driver.set_window_size(1920, 1080)
 
 df = pd.DataFrame()
-try:
-    with open(str(filePath)+"/../search_queries.txt", "r") as file:
+if os.path.isfile(str(filePath) + "/../temp/product_detail_links/"+"missing_wines.csv"):
+    df = pd.read_csv(
+        str(filePath) + "/../temp/product_detail_links/"+"missing_wines.csv", index_col=False)
 
-        for line in file.readlines():
 
-            queryString = line.strip()
-            driver.get("https://www.vivino.com/search/wines?q=" +
-                       str(queryString))
+with open(str(filePath)+"/../search.json", "r") as file:
+    json_file = json.load(file)
+    n = 0
+    n_len = len(json_file)
+    for line in json_file:
+        n += 1
+        print(n, "/", n_len)
+        try:
+            nId = int(line["local_id"])
+            dfId = df.local_id
 
-            element = WebDriverWait(driver, config.timeout).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, '//div[@class="search-results-list"]'))
-            )
-            html = element.get_attribute('innerHTML')
-            attributes = BeautifulSoup(
-                html, 'html.parser').find_all('a', href=True)
+            if(not dfId.isin([nId]).any()):
+                queryString = line["search_string"]
+                driver.get("https://www.vivino.com/search/wines?q=" +
+                           str(queryString))
 
-            for attr in attributes:
-                link = attr["href"]
-                if "/DE/de/wines/" in link:
-                    df = df.append({"datetime": time.time(),
-                                    "link":  link}, ignore_index=True)
+                element = WebDriverWait(driver, config.timeout).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//div[@class="search-results-list"]'))
+                )
+                html = element.get_attribute('innerHTML')
+                attributes = BeautifulSoup(
+                    html, 'html.parser').find_all('a', href=True)
 
-            time.sleep(1)
-except Exception as err:
-    print("Fehlermeldung:")
-    print(err)
-    traceback.print_exc()
+                i = 0
+                for attr in attributes:
+
+                    link = attr["href"]
+
+                    if "/wines/" in link:
+                        i += 1
+                        if i <= 3:
+                            df = df.append({"datetime": time.time(),
+                                            "link":  link, "local_id": line["local_id"]}, ignore_index=True)
+                time.sleep(5)
+        except Exception as err:
+            print("Fehlermeldung:")
+            print(err)
+            traceback.print_exc()
+
 
 df.drop_duplicates("link")
-df.to_csv(str(filePath) + "/../temp/product_detail_links/"+"missing_wines.csv")
+df.to_csv(str(filePath) + "/../temp/product_detail_links/" +
+          "missing_wines.csv", index_label="scrape_id")
+print("file saved")
 driver.close()
