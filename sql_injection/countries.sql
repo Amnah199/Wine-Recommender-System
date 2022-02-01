56a9e4a0d6cb52ae4b35dc64f6f08ffe2b1cb4d8
Zoran Lisovac <zoran.lisovac@gmail.com>
2022-02-01 10:47:53 +0100

    added script to even out the country names for vivino dataset

---
+++ sql_injection/countries.sql
@@ -0,0 +1,34 @@
+select distinct w.wine_country from wine w order by w.wine_country 
+
+
+-- France
+
+update wine set wine_country = 'Frankreich' where wine_country = 'France'
+
+-- Italy
+
+update wine set wine_country = 'Italien' where wine_country = 'Italy'
+
+-- US
+
+update wine set wine_country = 'Vereinigte Staaten' where wine_country = 'United States'
+
+-- Germany
+
+update wine set wine_country = 'Deutschland' where wine_country = 'Germany'
+
+-- Argentina
+
+update wine set wine_country = 'Argentinien' where wine_country = 'Argentina'
+
+-- Australia
+
+update wine set wine_country = 'Australien' where wine_country = 'Australia'
+
+-- Austria
+
+update wine set wine_country = 'Österreich' where wine_country = 'Austria'
+
+-- Spain
+
+update wine set wine_country = 'Spanien' where wine_country = 'Spain'
